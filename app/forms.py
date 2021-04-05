e3169f11a93c0728e53637ff3792f669ae7742bc
Rivaldo1123 <ramanlewis@gmail.com>
2021-04-05 00:02:11 -0500

    Minor Change

--- app/forms.py
+++ app/forms.py
@@ -9,18 +9,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
 
 class CreateUser(FlaskForm):
-    firstname = StringField('Firstname', validators=[InputRequired()])
-    lastname = StringField('Lastname', validators=[InputRequired()])
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
     age = StringField('Age', validators=[InputRequired(), Regexp("^\d+$")])
     email = StringField('Email Address', validators=[InputRequired(), Regexp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")])
     telephone = StringField('Telephone', validators=[InputRequired(), Regexp("^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$")])
     preference = SelectField(label='Meal Type', choices=[("Gain Weight", "Gain Weight"), ("Lose Weight", "Lose Weight")])
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
-    confirm = PasswordField('confirm', validators=[InputRequired()])
+    confirm = PasswordField('Retype Password', validators=[InputRequired()])
 
 class UploadForm(FlaskForm):
     title = StringField('Recipe Title', validators=[InputRequired(),Length(min=1, max=50)])
-    instructions = StringField('Instructions', validators=[InputRequired(),Length(min=1, max=250)])
+    instructions = TextAreaField('Instructions', validators=[InputRequired(),Length(min=1, max=250)])
     upload = FileField('Photo', validators=[FileRequired(),FileAllowed(['jpg', 'jpeg', 'png'], 
     'PLease select an Image!')])
